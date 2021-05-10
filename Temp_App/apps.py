4661da54bc14cc02a676e64cc44cf10aa9eedc37
SatinderK <kainthsatinder8@gmail.com>
2021-05-09 22:17:07 -0700

    User Template Files

---
+++ Temp_App/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TempAppConfig(AppConfig):
+    name = 'Temp_App'
---
+++ Temp_App/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+def index(request):
+    all_users = User.objects.all()
+    context= {
+        "all_users": all_users
+    }
+    return render(request,'index.html',context)
+
+def add_user(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        age = request.POST['age']
+        User.objects.create(first_name=first_name, last_name=last_name, email=email, age=age)
+    return redirect("/")
